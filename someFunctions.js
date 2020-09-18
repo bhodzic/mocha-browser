1203238c54109bf6a4a6fb9509624094fb8af437
bhodzic <hodzic.bojan@gmail.com>
2020-09-18 11:53:46 +0200

    allowing all integers for pow and adding palindrome tests

--- someFunctions.js
+++ someFunctions.js
@@ -1,6 +1,11 @@
 function pow(x, n) {
-    if ((n < 0) || (Math.round(n) != n)) return NaN;
+    if (Math.round(n) != n) return NaN;
     if (n == 1) return x;
+    if (n == 0) return 1;
+    if (n < 0) {
+        if (x == 0) return NaN;
+        return (1 / (pow(x, -1 * n)));
+    }
     return x * pow(x, n - 1);
 }
 
@@ -11,9 +16,9 @@ function factorial(x) {
 }
 
 function palindrome(x) {
-    if(!x.length) return false;
-    for(let i=0;i<x.length/2;i++){
-        if(x[i]!=x[x.length-1-i]){
+    if (!x.length) return false;
+    for (let i = 0; i < x.length / 2; i++) {
+        if (x[i] != x[x.length - 1 - i]) {
             return false;
         }
     }
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 
 describe("pow", function () {
-    describe("raises x to power 3", function () {
+    describe("raises x to power n (n is integer)", function () {
 
         function makeTest(x) {
             let expected = x * x * x;
@@ -9,14 +9,47 @@ describe("pow", function () {
             });
         }
 
-        for (let x = 1; x <= 5; x++) {
+        for (let x = 2; x <= 4; x++) {
             makeTest(x);
         }
+
+        it(`5 in the power 0 is 1`, function () {
+            assert.equal(pow(5, 0), 1);
+        });
+
+
+        it(`2 in the power 1.5 is NaN`, function () {
+            assert.equal(Number.isNaN(pow(2, 1.5)), true);
+        });
+
+        it(`2 in the power -1 is 0.5`, function () {
+            assert.equal(pow(2, -1), 0.5);
+        });
+
+        it(`2 in the power -3.5 is NaN`, function () {
+            assert.equal(Number.isNaN(pow(2, -3.5)), true);
+        });
+
+        it(`0 in the power 15 is 0`, function () {
+            assert.equal(pow(0, 15), 0);
+        });
+
+        it(`0 in the power -15 is NaN`, function () {
+            assert.equal(Number.isNaN(pow(0, -15)), true);
+        });
+
+        it(`0 in the power 0 is 1`, function () {
+            assert.equal(pow(0, 0), 1);
+        });
     });
 });
 
 describe("factorial", function () {
     describe("returns factorial of x", function () {
+
+        it(`0 factorial is 1`, function () {
+            assert.equal(factorial(0), 1);
+        });
 
         it(`3 factorial is 6`, function () {
             assert.equal(factorial(3), 6);
@@ -28,6 +61,45 @@ describe("factorial", function () {
 
         it(`5 factorial is 120`, function () {
             assert.equal(factorial(5), 120);
+        });
+
+        it(`-5 factorial is NaN`, function () {
+            assert.equal(Number.isNaN(factorial(-5)), true);
+        });
+
+        it(`-3.5 factorial is NaN`, function () {
+            assert.equal(Number.isNaN(factorial(-3.5)), true);
+        });
+
+        it(`3.5 factorial is NaN`, function () {
+            assert.equal(Number.isNaN(factorial(3.5)), true);
+        });
+
+    });
+});
+
+
+describe("palindrome", function () {
+    describe("returns true if input is palindrome", function () {
+
+        it(`abba is palindrome`, function () {
+            assert.equal(palindrome('abba'), true);
+        });
+
+        it(`google is not palindrome`, function () {
+            assert.equal(palindrome('google'), false);
+        });
+
+        it(`11 22 11 is palindrome`, function () {
+            assert.equal(palindrome('11 22 11'), true);
+        });
+
+        it(`112 is not palindrome`, function () {
+            assert.equal(palindrome('112'), false);
+        });
+
+        it(`radar is palindrome`, function () {
+            assert.equal(palindrome('radar'), true);
         });
 
     });
